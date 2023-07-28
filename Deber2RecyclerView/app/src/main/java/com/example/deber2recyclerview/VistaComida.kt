55f44a084ea20e781029e874c097d4837bb9d88f
Nicolás Toscano <nicolas.toscano@epn.edu.ec>
2023-07-28 15:03:11 -0500

    Deber 2 Recycler View Final

--- Deber2RecyclerView/app/src/main/java/com/example/deber2recyclerview/VistaComida.kt
+++ Deber2RecyclerView/app/src/main/java/com/example/deber2recyclerview/VistaComida.kt
@@ -11,25 +11,28 @@ import com.example.deber2recyclerview.BD.BaseDeDatos
 class VistaComida : AppCompatActivity() {
 
     private lateinit var binding: RecyclerViewComidaBinding
-
+    private lateinit var nombreCategoria: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = RecyclerViewComidaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        nombreCategoria = intent.getStringExtra("Categoria_nombre") ?: ""
         initRecyclerView()
     }
 
     private fun initRecyclerView() {
         binding.recyclerViewComida.layoutManager = LinearLayoutManager(this)
-
-        // Combina todas las listas de comidas de cada categoría en una sola lista
+        // Encuentra la categoría seleccionada
+        val categoriaSeleccionada =
+            BaseDeDatos.listaCategorias.find { it.nombreCategoria == nombreCategoria }
         val listaComidas = mutableListOf<Comida>()
-        for (categoria in BaseDeDatos.listaCategorias) {
-            listaComidas.addAll(categoria.comida)
+        // Verifica si se encontró la categoría seleccionada y agrega sus comidas a la lista
+        categoriaSeleccionada?.let {
+            listaComidas.addAll(it.comida)
         }
-
         val comidaAdapter = ComidaAdapter(listaComidas)
         binding.recyclerViewComida.adapter = comidaAdapter
     }
+
 }
 
